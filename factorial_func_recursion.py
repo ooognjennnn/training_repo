41519c9ee8bcc80ab551e486e7912ba477acd092
ooognjennnn <ognjendj97@gmail.com>
2025-03-13 18:38:09 +0100

    Modified factorial func using recursion method

--- factorial_func_recursion.py
+++ factorial_func_recursion.py
@@ -1,12 +1,12 @@
 #factorial_func
 
-def factorial(n):
+def factorial_recursion(n):
     if n < 0:
         return "Factorial is not defind for negative numbers"
     if n == 0 or n == 1:
         return 1
     else:
-        return n * factorial(n-1)
-print(factorial(5))
+        return n * factorial_recursion(n-1)
+print(factorial_recursion(5))
 
         
