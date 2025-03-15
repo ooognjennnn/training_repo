9f1c47e8a3c81cbfb659300d7375230b41d0d3da
ooognjennnn <ognjendj97@gmail.com>
2025-03-15 07:30:02 +0100

    Updated yes_or_no function to use set instead of list

--- task_1/yes_no_func.py
+++ task_1/yes_no_func.py
@@ -1,16 +1,13 @@
-def yes_or_no(collections: list) -> None:
-    seen = []
-    for char in collections:
-        if char in seen:
+def yes_or_no(collection: list) -> None:
+    seen = set()
+    for item in collection:
+        if item in seen:
             print("Yes")
         else:
             print("No")
-            seen.append(char)
-
-yes_or_no([1,2,3,1,2,4,2])
+            seen.add(item)
 
 
-
-                
+yes_or_no([1, 2, 3, 1, 2, 4, 2])
     
 
